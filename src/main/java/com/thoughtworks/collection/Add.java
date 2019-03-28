6955eb0cb0039ba07d1a0ec506dfbba5fb217da8
mazerong <chdmazr@126.com>
2019-03-28 23:54:41 +0800

    should get triple add two

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -23,11 +23,27 @@ public class Add {
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int i,j,sum=0;
+        if (leftBorder <= rightBorder)
+        {
+            if(leftBorder % 2 == 1) i = leftBorder; else i = leftBorder + 1;
+            if(rightBorder % 2 == 1) j = rightBorder; else j = rightBorder - 1;
+        }else
+        {
+            if(rightBorder % 2 == 1) i = rightBorder; else i = rightBorder + 1;
+            if(leftBorder % 2 == 1) j = leftBorder; else j = leftBorder - 1;
+        }
+        for(; i <= j; i += 2)
+            sum += i;
+        return sum;
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int sum =0;
+        for (Integer temp : arrayList) {
+            sum += temp*3+2;
+        }
+        return  sum;
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
